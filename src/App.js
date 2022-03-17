5cfc620c47b387f6a7e89456f1104f63f6ee189a
Marcelo <marcelo.cavalcante@ebac.art.br>
2022-03-17 07:56:08 -0300

    code together

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import './App.css';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
